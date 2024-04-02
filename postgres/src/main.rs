66546664f6f27ce36d04b9feffe7d2593e4cf9e6
HarijayarajS <128171168+HarijayarajS@users.noreply.github.com>
2024-04-03 00:36:04 +0530

    Update main.rs

--- postgres/src/main.rs
+++ postgres/src/main.rs
@@ -118,3 +118,109 @@ async fn main() -> Result<(), Error> {
     println!("{:#?}", items);
     Ok(())
 }
+
+use chrono::{DateTime, Datelike, Local, NaiveDate, NaiveDateTime, Utc};
+use tokio_postgres::{NoTls, Error};
+
+#[tokio::main] 
+async fn main() -> Result<(), Error> {
+    // Configure the connection to the PostgreSQL database
+    let (client, connection) = tokio_postgres::connect(
+        "host=localhost user=postgres dbname=postgres password=password",
+        NoTls,
+    )
+    .await?;
+
+    // Spawn a task to process the connection asynchronously
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    
+    let start = NaiveDate::from_ymd_opt(2024,04,1).unwrap();
+    let end = NaiveDate::from_ymd_opt(2024,04,03).unwrap();
+
+    #[derive(Debug)]
+    struct StatusLogEmployeeItem{
+        id: i64,
+        log_date: String,
+        log_type: String,
+        time_in: String,
+        time_out: String,
+        break_time: i64,
+        lunch_time: i64,
+        title: String
+    }
+    let mut all_items: Vec<StatusLogEmployeeItem> =  vec![];
+
+    fn datetime_to_i64(date: NaiveDate) -> i64 {
+        if let Some(date) = date.and_hms_opt(0, 0, 0){
+            date.and_utc().timestamp()
+        }else {
+            0
+        }
+    }
+
+    fn datetime_utc_to_i64(datetime_utc: DateTime<Utc>) -> i64 {
+        let date: NaiveDate = datetime_utc.date_naive();
+        if let Some(date) = date.and_hms_opt(0, 0, 0){
+            date.and_utc().timestamp()
+        }else {
+            0
+        }
+    }
+
+    fn datetime_utc_to_naive(datetime_utc: DateTime<Utc>) -> DateTime<Local> {
+        datetime_utc.with_timezone(&Local)
+    }
+
+    for date in start.iter_days().take_while(|&d| d <= end) {
+    // Mark Sundays as holidays
+            let mut title = "".to_string();
+            let mut log_type = "working".to_string();
+            if date.weekday() == chrono::Weekday::Sun {
+                log_type = "Holiday".to_string();
+                title = "Weekend - Sunday".to_string();
+            }
+            all_items.push(StatusLogEmployeeItem{ 
+                id: datetime_to_i64(date), 
+                log_date: date.format("%Y-%m-%d").to_string(), 
+                log_type, 
+                time_in: "".to_string(), 
+                time_out:"".to_string(), 
+                break_time: 0, 
+                lunch_time: 0, 
+                title 
+            })
+        }
+
+    // Perform a simple query to select some data from a table
+    let rows = client
+        .query("SELECT * FROM employee_status_log WHERE DATE(created_on) >= $1 AND 
+            DATE(created_on) <= $2 AND employee_id = 1", &[&start,&end])
+        .await?;
+        for row in &rows {
+            let db_id: chrono::DateTime<Utc> = row.get("created_on");
+            let id = datetime_utc_to_i64(db_id);
+            let val = all_items.iter_mut().find(|ele| ele.id == id);
+            if let Some(value) = val {
+                if row.get::<_,String>("status") == *"break-in"{
+                    value.break_time += row.get::<_,i64>("time_taken")
+                }
+                if row.get::<_,String>("status") == *"lunch-in"{
+                    value.lunch_time += row.get::<_,i64>("time_taken")
+                }
+                if row.get::<_,String>("status") == *"day-out"{
+                    let time_in = row.get::<_,chrono::DateTime<Utc>>("created_on");
+                    value.time_in =  datetime_utc_to_naive(time_in).format("%H:%M").to_string();
+                    value.time_out = row.get::<_,NaiveDateTime>("modified_on").format("%H:%M").to_string();
+
+                }
+            }
+
+        }
+println!("{:#?}",all_items);
+    Ok(())
+}
+
