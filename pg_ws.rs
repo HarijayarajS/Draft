0ace9f59db54238473b0bca21c616b7b937cace7
HarijayarajS <128171168+HarijayarajS@users.noreply.github.com>
2025-05-08 18:46:22 +0530

    Update pg_ws.rs

--- pg_ws.rs
+++ pg_ws.rs
@@ -31,3 +31,76 @@ async fn main() {
             
     }
 }
+
+
+use futures::{stream, StreamExt};
+use futures::{FutureExt, TryStreamExt};
+use std::env;
+use tokio_postgres::{connect, NoTls};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[tokio::main]
+async fn main() {
+    // PostgreSQL connection.
+    let (client, mut connection) = tokio_postgres::connect("host=localhost user=postgres dbname=", NoTls).await.unwrap();
+
+
+    // Make transmitter and receiver.
+    let (tx, rx) = futures_channel::mpsc::unbounded();
+    let stream =
+        stream::poll_fn(move |cx| connection.poll_message(cx)).map_err(|e| panic!("{}", e));
+    let connection = stream.forward(tx).map(|r| r.unwrap());
+    tokio::spawn(connection);
+
+
+    // Wait for notifications in seperate thread.
+    tokio::spawn(async move {
+    let notifications = rx
+        .filter_map(|m| match m {
+            tokio_postgres::AsyncMessage::Notification(n) => {
+                println!("Notification {:?}", n);
+                futures_util::future::ready(Some(n))
+            },
+            _ => futures_util::future::ready(None),
+        })
+        .collect::<Vec<_>>().await;
+
+        // All notifications?
+        println!("All notifications {:?}", notifications);
+    });
+   
+    // Execute listen/notify
+    match client
+        .batch_execute(
+            "LISTEN test_notifications;
+             NOTIFY test_notifications, 'hello';
+             NOTIFY test_notifications, 'world';",
+        )
+        .await
+        {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error {}", e);
+            }
+        }
+       
+    // Execute random query.
+    let query =
+        client
+        .query("
+                SELECT order_id FROM history LIMIT 1 
+            ", &[]).await;
+
+    match query {
+        Ok(q) => {
+            let r = q[0].get::<_, &str>("order_id");
+            println!("r {}", r);
+        },
+        Err(e) => {
+            return;
+        }
+    }
+}
+
+
