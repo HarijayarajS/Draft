c662687a1875794a67977fd35e617073dcb8b480
HarijayarajS <128171168+HarijayarajS@users.noreply.github.com>
2025-10-14 12:53:43 +0530

    Create floor.sql

---
+++ floor.sql
@@ -0,0 +1,11 @@
+INSERT INTO floorplan (doc_id, org_id, venue_id, title, no_rooms) VALUES
+('flp_tv1_ground', 'test-org-1', 'test-venue-1', 'Exhibition Hall - Ground Floor', 8),
+('flp_tv1_mezz', 'test-org-1', 'test-venue-1', 'Mezzanine Level & Suites', 12),
+('flp_tv6_conf', 'test-org-1', 'test-venue-6', 'Conference Center Layout A', 15),
+('flp_tv6_gala', 'test-org-1', 'test-venue-6', 'Gala Dinner Setup', 3),
+('flp_tv11_ballrm', 'test-org-11', 'test-venue-11', 'Grand Ballroom Floorplan', 2),
+('flp_tv2_arena', 'test-org-2', 'test-venue-2', 'Main Arena & Lower Bowl', 1),
+('flp_tv3_garden', 'test-org-3', 'test-venue-3', 'Garden View Banquet Hall', 4),
+('flp_tv4_rooftop', 'test-org-4', 'test-venue-4', 'Rooftop Terrace & Lounge', 2),
+('flp_tv5_east_wing', 'test-org-5', 'test-venue-5', 'Business Center - East Wing', 22),
+('flp_tv5_west_wing', 'test-org-5', 'test-venue-5', 'Business Center - West Wing', 18);
